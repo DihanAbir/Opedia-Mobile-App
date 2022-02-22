e79c0b74aa597a02679aab7f734808c9bb5c7392
DihanAbir <nahid.muradabir@gmail.com>
2022-02-23 01:51:35 +0600

    Added full app for opedia

---
+++ src/data/data.js
@@ -0,0 +1,157 @@
+const insuranceData = [
+  {
+    id: 1,
+    title: "Software Development",
+    color: "#ffdfd2",
+    image: require("./../assets/car-image.png"),
+    description:
+      "Prepare for your family's future in the case of an unexpected tragedy. Insurance helps you.",
+    subPart: [
+      {
+        title: "Cloud Computing",
+        subPart: [
+          {
+            title: "Cloud1",
+            url: "truck",
+            image: require("./../assets/truck.png"),
+            description:
+              "Construction is a general term the art and science to form",
+          },
+          {
+            title: "Cloud1",
+            url: "bus_van",
+            image: require("./../assets/Bus.png"),
+            description:
+              "Construction is a general term the art and science to form",
+          },
+          {
+            title: "Cloud1",
+            url: "car",
+            image: require("./../assets/private.png"),
+            description:
+              "Construction is a general term the art and science to form",
+          },
+        ],
+        url: "commercial",
+        image: require("./../assets/commercial.png"),
+        description:
+          "Construction is a general term the art and science to form",
+      },
+      {
+        title: "Cloud1",
+        url: "private",
+        image: require("./../assets/private.png"),
+        description:
+          "Construction is a general term the art and science to form",
+      },
+      {
+        title: "Cloud1",
+        url: "three-wheeler",
+        image: require("./../assets/threeWheel.png"),
+        description:
+          "Construction is a general term the art and science to form",
+      },
+      {
+        title: "Cloud1",
+        url: "two-wheeler",
+        image: require("./../assets/twowheeler.png"),
+        description:
+          "Construction is a general term the art and science to form",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Web & Software",
+    color: "#d4e5fb",
+    image: require("./../assets/life.png"),
+    description:
+      "Prepare for your family's future in the case of an unexpected tragedy. Insurance helps you.",
+    subPart: [
+      {
+        title: "Web & Software",
+        url: "permanent",
+        image: require("./../assets/permanent-life.png"),
+        description:
+          "Prepare for your family's future in the case of an unexpected tragedy. Insurance Helps you.",
+      },
+      {
+        title: "Web & Software",
+        url: "term",
+        image: require("./../assets/team-life.png"),
+        description:
+          "Prepare for your family's future in the case of an unexpected tragedy. Insurance Helps you.",
+      },
+      {
+        title: "Web & Software",
+        url: "group",
+        image: require("./../assets/Group-life.png"),
+        description:
+          "Prepare for your family's future in the case of an unexpected tragedy. Insurance Helps you.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Product Designing",
+    color: "#fdedc9",
+    image: require("./../assets/driver.png"),
+    description:
+      "Insurance plans that cover trip cancellation, travel medicines and many more from your loss.",
+    subPart: [
+      {
+        title: "Product Designing",
+        url: "single-trip",
+        image: require("./../assets/single-trip.png"),
+        description:
+          "Insurance that plans cover trip cancellation, travel medical and many more from your loss.",
+      },
+      {
+        title: "Product Designing",
+        url: "multi-trip",
+        image: require("./../assets/multi-trip.png"),
+        description:
+          "Insurance that plans cover trip cancellation, travel medical and many more from your loss.",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Web Designing",
+    color: "#ffdaed",
+    image: require("./../assets/health.png"),
+    description:
+      "Health is the most important thing that we should protect. Health insurance will take care.",
+    subPart: [
+      {
+        title: "Web Designing",
+        url: "PPO",
+        image: require("./../assets/ppo.png"),
+        description:
+          "PPO insurance is designed to protect yourself and others agains many various risks",
+      },
+      {
+        title: "Web Designing",
+        url: "HMO",
+        image: require("./../assets/hmo.png"),
+        description:
+          "HMO for your family's future in the case of an unexpected tragedy. Insurance helps you.",
+      },
+      {
+        title: "Web Designing",
+        url: "point-of-service",
+        image: require("./../assets/point-of-service.png"),
+        description:
+          "Point-of-service life Insurance plans that cover trip cancellation, travel medicines and many more from your loss.",
+      },
+      {
+        title: "Web Designing",
+        url: "health-saving",
+        image: require("./../assets/health-saving.png"),
+        description:
+          "Point-of-service life Insurance plans that cover trip cancellation, travel medicines and many more from your loss.",
+      },
+    ],
+  },
+];
+export default insuranceData;
